7cb0e1d7452c4596fd665b53e4e9273e53977066
Brandyn Friedly <bfriedly@planning.nyc.gov>
2020-05-11 15:22:44 -0400

    Add component state & opt in to UI reactivity
    This commit adds a component method to update the state of the compont when a user switches the selected user in the dropdown, and subscribes the UI to changes in this tracked property.
    
    Difference between Ember classic and Octane — ember classic promoted 2-way data binding, Octane promotes 1-way data binding so we have to explicitly declare the reactive property.

--- app/components/login-form.js
+++ app/components/login-form.js
@@ -1,11 +1,25 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class LoginFormComponent extends Component {
+
+  // property to keep track of the user selection in the UI
+  @tracked // Octane 1-way data binding.  This opts in to listen to changes
+  userId = null;
 
   loginAsUserId(val) {
     console.log('UserID: ', val)
   }
+
+  /**
+   * @param {Event & { target: HTMLSelectElement }} event
+   */
+  @action 
+  onSelectChanged(event) {
+    this.userId = event.target.value;
+  }
+
   /**
    * 
    * @param {Event & {target: HTMLFormElement}} event 
