200a85ae950c8e2f817725c91f0082d46e1d2b29
Brandyn Friedly <bfriedly@planning.nyc.gov>
2020-05-12 15:31:21 -0400

    Create container component logic, pass to child
    This commit sets up the actions to create a new message in the database.  The api handler is in the container component, which gets yielded through a hash.
    
    This then gets passed to channel-footer (which is a mostly presentational component except for some minor internal component state).  It’s expecting this action to be passed as an arg, which we do.
    
    Also some minor UI state on the button logic using the getter for whether or not the footer button should be disabled.
    
    The thinking here is that you want to separete pretty clearly which components are going to be smart and stateful (our chat-contianer) and which ones are mostly presentational (our channel-footer).

--- app/components/chat-container.js
+++ app/components/chat-container.js
@@ -2,10 +2,17 @@ import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import fetch from 'fetch';
+import AuthService from 'shlack/services/auth';
+import { inject as service } from '@ember/service';
 
 export default class ChatContainerComponent extends Component {
   @tracked
   messages = [];
+
+  /**
+   * @type {AuthService}
+   */
+  @service auth;
 
   @action
   async loadMessages() {
@@ -21,5 +28,25 @@ export default class ChatContainerComponent extends Component {
     // include the existing messages, add new messages
     // note we aren't deduping these messages
     this.messages = [...(await response.json())];
+  }
+
+  @action
+  async createMessage(body) {
+    const {
+      channel: { id: channelId, teamId },
+    } = this.args;
+
+    const response = await fetch('/api/messages', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        teamId,
+        channelId,
+        userId: this.auth.currentUserId,
+        body,
+      }),
+    });
   }
 }
