c6b4fe34d039387148dce17824c46acaf3ae2023
Brandyn Friedly <bfriedly@planning.nyc.gov>
2020-05-12 12:35:11 -0400

    remove debugger

--- app/components/login-form.js
+++ app/components/login-form.js
@@ -2,10 +2,9 @@ import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
-import AuthService from 'shlack/services/auth'
+import AuthService from 'shlack/services/auth';
 
 export default class LoginFormComponent extends Component {
-
   // property to keep track of the user selection in the UI
   @tracked // Octane 1-way data binding.  This opts in to listen to changes
   userId = null;
@@ -14,7 +13,7 @@ export default class LoginFormComponent extends Component {
    * @type {AuthService}
    */
   @service
-  auth
+  auth;
 
   // javascript getter for derived state
   get isDisabled() {
@@ -24,21 +23,20 @@ export default class LoginFormComponent extends Component {
   /**
    * @param {Event & { target: HTMLSelectElement }} event
    */
-  @action 
+  @action
   onSelectChanged(event) {
     this.userId = event.target.value;
   }
 
   /**
-   * 
-   * @param {Event & {target: HTMLFormElement}} event 
+   *
+   * @param {Event & {target: HTMLFormElement}} event
    */
   @action
   onLoginFormSubmit(event) {
     const { target } = event;
     const val = target.querySelector('select').value;
     event.preventDefault();
-    debugger;
     this.auth.loginWithUserId(val);
   }
 }
